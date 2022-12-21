57e807ce4fb64d9422bc674cb3ca60c4f695a2e4
Jens Andreasson <jens.andreasson@ntig.se>
2022-12-21 11:40:03 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -8,38 +8,37 @@ let song = [
     {r: false, n: Note.G, d: q + e}, // last
     {r: false,n: Note.G, d: e}, // chri-
     {r: false,n: Note.G, d: e}, // -st
-    {r: false,n: Note.F, d: e}, // -mas
-    {r: false,n: Note.F, d: e},
-    {r: false,n: Note.C, d: e}, // I
+    {r: false,n: Note.F, d: q}, // -mas
+    {r: false,n: Note.C, d: e},
+    // second bar
+    {r: false,n: Note.G, d: e}, // I
     {r: false,n: Note.G, d: e}, // gave
-    {r: false,n: Note.G, d: e}, // you
-    {r: false,n: Note.A, d: q}, // my
-    {r: false,n: Note.F, d: q + e}, // heart
+    {r: false,n: Note.A, d: q}, // you
+    {r: false,n: Note.F, d: q + e}, // my
+    {r: false,n: Note.C, d: e}, // heart
     // third bar
-    {r: false,n: Note.D, d: e}, // but
+    {r: false,n: Note.G, d: e}, // but
     {r: false,n: Note.G, d: e}, // the 
-    {r: false,n: Note.G, d: e}, // very
     {r: false, n: Note.A, d: q }, // next
     {r: false, n: Note.F, d: q + e}, // day
-    //{ r: true, n: Note.D, d: e }, // rest
     {r: false, n: Note.F, d: e }, // you
+    // fourth
     {r: false, n: Note.E, d: e }, // gave
     {r: false, n: Note.F, d: e }, // it
     { r: false, n: Note.E, d: e }, // a
     { r: false, n: Note.D, d: e }, // way
-    { r: false,n: Note.D, d:h+q }, // y---
+    { r: false,n: Note.D, d:h }, // y---
     // fifth bar
-   // {r: true, n: Note.D, d: q}, // rest
     { r: false,n: Note.A, d: q+e }, // this
     { r: false,n: Note.G, d: e }, // yea
     { r: false,n: Note.G, d: q }, // r---
-//    { r: true, n: Note.D, d: e },
     { r: false,n: Note.D, d: e }, // to
     { r: false,n: Note.D, d: e}, // save
+    // sixth
     { r: false, n: Note.A, d: e},
-    {r: false, n: Note.D, d: e},
-    { r: false,n: Note.Bb, d: e }, // me
-    { r: false,n: Note.A, d: e }, // from
+    {r: false, n: Note.Bb, d: e},
+    { r: false,n: Note.A, d: e }, // me
+    { r: false,n: Note.G, d: e }, // from
     { r: false,n: Note.G, d: q + e }, // tears
     { r: false, n: Note.F, d: e },
     // sevent bar
@@ -48,13 +47,13 @@ let song = [
     { r: false, n: Note.F, d: e }, // it
     { r: false, n: Note.E, d: e }, // to --
     { r: false, n: Note.E, d: e }, // -- some 
-    { r: false, n: Note.F, d: e }, // some
-    { r: false, n: Note.F, d: q }, // one
+    { r: false, n: Note.F, d: q }, // some
     { r: false, n: Note.E, d: e }, // spec
+    // eigth
     { r: false, n: Note.E, d: e }, // cial
     { r: false, n: Note.C, d: e }, // cial
-    {r: false, n: Note.C, d: h+q},
-    { r: true, n: Note.C, d: h }
+    {r: false, n: Note.C, d: h},
+    { r: true, n: Note.C, d: q }
 ]
 basic.forever(function () {
     song.forEach(part => {
